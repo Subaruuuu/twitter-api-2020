2562addef80d063284a408c0312187b71e0a6016
subaruuuu <meteorbs13@gmail.com>
2021-03-13 15:30:33 +0800

    modify socket.io adjustment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/upload', express.static(__dirname + '/upload'))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
 const swaggerUi = require('swagger-ui-express')
@@ -26,7 +26,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => res.send('請使用API接口'))
 
-socketServer.listen(port, () => console.log(`Socket.io server listening on port 3000`))
+socketServer.listen(port, () => console.log(`Socket.io server listening on port ${port}`))
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('./routes')(app)
