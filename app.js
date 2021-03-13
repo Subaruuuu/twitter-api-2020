7a345a58151a7e2e770814617f5d980b2b8cece3
subaruuuu <meteorbs13@gmail.com>
2021-03-13 15:44:55 +0800

    add cors settings for socket.io

--- app.js
+++ app.js
@@ -10,14 +10,19 @@ const cors = require('cors')
 const app = express()
 const http = require('http')
 const socketServer = http.createServer(app)
-const socket = require('socket.io')
-const io = socket(socketServer)
+// const socket = require('socket.io')
 const port = process.env.PORT || 3000
+const io = require("socket.io")(socketServer, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"]
+  }
+})
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/upload', express.static(__dirname + '/upload'))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
 const swaggerUi = require('swagger-ui-express')
