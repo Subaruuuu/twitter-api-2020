99caafe5f50b689874069d048aada2f4c8c823ad
subaruuuu <meteorbs13@gmail.com>
2021-03-13 16:01:59 +0800

    modification for test message

--- routes/io.js
+++ routes/io.js
@@ -6,6 +6,10 @@ module.exports = (io) => {
 
     socket.broadcast.emit('message', 'A user has join the chat')
 
+    socket.on('judy', judy => {
+      console.log(judy)
+    })
+
     // runs when client disconnects
     // 使用者離開後，針對頻道內所有人發出的離線訊息
     socket.on('disconnect', () => {
